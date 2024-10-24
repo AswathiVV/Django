859f54a99a5ff104458ad60db1b9f59b27cf9e8a
AswathiVV <aswathi.v.v.916@gmail.com>
2024-10-24 13:36:19 +0530

    add/delete

--- std_mgt_model/app/urls.py
+++ std_mgt_model/app/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('z',views.fun1),
     path('x',views.disp_std),
-    path('g',views.fun), 
+    # path('g',views.fun),
+    path('add_std',views.add_std),
+    path('edit_std/<id>',views.edit_std)
 ]
--- std_mgt_model/app/views.py
+++ std_mgt_model/app/views.py
@@ -9,56 +9,48 @@ def disp_std(req):
     data=Student.objects.all()
     return render(req,'display_std.html',{'data':data})
 
-# todo=[{'roll_no':'1','name':'Anu','age':'20','email':'anu@gmail.com','phno':'1234567890'}]
-# def fun(request):
-#     if request.method=='POST':
-#         roll_no=request.POST['roll_no']
-#         name=request.POST['name']
-#         age=request.POST['age']
-#         email=request.POST['email']
-#         phno=request.POST['phno']
-        
-#         todo.append({'roll_no':roll_no,'name':name,'age':age,'email':email,'phno':phno})
-#         print(todo)
-#         return redirect(fun)
-#     return render(request,'std_add.html',{'todo':todo})
-
-# from django.shortcuts import render, redirect
 
 todo = [{'roll_no': '1', 'name': 'Anu', 'age': '20', 'email': 'anu@gmail.com', 'phno': '1234567890'}]
 
-def fun(request):
-    if request.method == 'POST':
-        roll_no = request.POST.get('roll_no')
-        name = request.POST.get('name')
-        age = request.POST.get('age')
-        email = request.POST.get('email')
-        phno = request.POST.get('phno')
+# def fun(request):
+#     if request.method == 'POST':
+#         roll_no = request.POST('roll_no')
+#         name = request.POST('name')
+#         age = request.POST('age')
+#         email = request.POST('email')
+#         phno = request.POST('phno')
         
-        todo.append({'roll_no': roll_no, 'name': name, 'age': age, 'email': email, 'phno': phno})
-        print(todo)
-        return redirect(fun)  # Replace with your view name
+#         todo.append({'roll_no': roll_no, 'name': name, 'age': age, 'email': email, 'phno': phno})
+#         print(todo)
+#         return redirect(fun)  
     
-    return render(request, 'std_add.html', {'todo': todo})
+#     return render(request, 'std_add.html', {'todo': todo})
 
+def add_std(req):
+    if req.method == 'POST':
+        roll= req.POST['roll']
+        std_name = req.POST['name']
+        std_age = req.POST['age']
+        std_email = req.POST['email']
+        std_phno = req.POST['phno']
+        data=Student.objects.create(roll_no=roll,name=std_name,age=std_age,email=std_email,phno=std_phno)
+        data.save()
+        return redirect(disp_std)  
+    else:
+        return redirect(disp_std)
+    
+def edit_std(req,id):
+    data=Student.objects.get(pk=id)
+    if req.method=='POST':
+        roll= req.POST['roll_no']
+        std_name = req.POST['name']
+        std_age = req.POST['age']
+        std_email = req.POST['email']
+        std_phno = req.POST['phno']
+        Student.objects.filter(pk=id).update(roll_no=roll,name=std_name,age=std_age,email=std_email,phno=std_phno)
+        return redirect(disp_std)  
+    else:
+        return render(req,'edit_std.html',{'data':data})
+    
 
-# def form2(request,id):
-#     roll_no=' '
-#     for i in todo:
-#         if i['id']==id:
-#             task=i
-#     if request.method=='POST':
-#         roll_no=request.POST['roll_no']
-#         name=request.POST['name']
-#         age=request.POST['age']
-#         email=request.POST['email']
-#         phno=request.POST['phno']
-        
-#         task['roll_no']=roll_no
-#         task['name']=name
-#         task['age']=age
-#         task['email']=email
-#         task['phno']=phno
-        
-#         return redirect(fun)
-#     return render(request,'std_add.html',{'data':task})
+    
